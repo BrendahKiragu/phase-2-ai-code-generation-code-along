b0a85eb5b18124713201ffcbfc40eb5b8a9521c6
Brendah Mwihaki Kiragu <brendakiragu02@gmail.com>
2024-08-09 14:46:30 +0300

    completes test

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Your code here
+/** A book object representation.
+ * @typedef {Book} Book
+ * @property{string} title - Title of the book
+ * @property{string} author - Author of the book
+ * @property{string} genre - the genre of the book
+ */
+
+/**
+ * @param {string} genre - the genre of the book
+ * @param {string} books - an array of books object
+ * returns {Book []} An array of book objects matching the specified genre
+ */
+export function filterByGenre(genre, books) {
+
+  return books.filter(book => book.genre === genre);
+}
+
